4bbcce8e5ff31d30bbb2021863b66d5de0d7ce42
Pintta <69728770+Pintta@users.noreply.github.com>
2023-08-29 08:57:06 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-Pintta.Functions.CreateCallback('qb-NaturalTrafficLights:s:setLight', function(coords)
+QBCore.Functions.CreateCallback('qb-NaturalTrafficLights:s:setLight', function(coords)
     TriggerClientEvent('qb-NaturalTrafficLights:c:setLight', -1, coords)
 end)
