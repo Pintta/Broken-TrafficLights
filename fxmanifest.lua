8bc5a8aea8be57bf0275487780af2343498c1bb9
Pintta <69728770+Pintta@users.noreply.github.com>
2024-04-15 19:05:41 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-client_script 'client.lua'
-server_script 'server.lua'
+--client_script 'client.lua' -- USING QBCORE
+--server_script 'server.lua' -- USING QBCORE
+
+client_script 'client-esx.lua' -- USING ESX
+server_script 'server-esx.lua' -- USING ESX
