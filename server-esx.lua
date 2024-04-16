c99b5b097093c68f244bd849ec3cf3cdaad25417
Pintta <69728770+Pintta@users.noreply.github.com>
2024-04-16 14:16:41 +0300

    Update server-esx.lua

--- server-esx.lua
+++ server-esx.lua
@@ -1,3 +1,3 @@
-RegisterNetEvent('qb-NaturalTrafficLights:s:setLight', function(coords)
-    TriggerClientEvent('qb-NaturalTrafficLights:c:setLight', -1, coords)
+RegisterNetEvent('Broken-TrafficLights:s:setLight', function(coords)
+    TriggerClientEvent('Broken-TrafficLights:c:setLight', -1, coords)
 end)
