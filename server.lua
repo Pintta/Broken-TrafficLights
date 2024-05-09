8c7f2d3764c94ff5a627c5d38f2a37298089fc99
Pintta <69728770+Pintta@users.noreply.github.com>
2024-05-09 22:43:38 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,17 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+if switcher.qbcore then
+    local QBCore = exports['qb-core']:GetCoreObject()
+    
+    QBCore.Functions.CreateCallback('qb-NaturalTrafficLights:s:setLight', function(coords)
+        TriggerClientEvent('qb-NaturalTrafficLights:c:setLight', -1, coords)
+    end)
+else
+    return
+end
 
-QBCore.Functions.CreateCallback('qb-NaturalTrafficLights:s:setLight', function(coords)
-    TriggerClientEvent('qb-NaturalTrafficLights:c:setLight', -1, coords)
-end)
+if switcher.esx then
+    RegisterNetEvent('Broken-TrafficLights:s:setLight', function(coords)
+        TriggerClientEvent('Broken-TrafficLights:c:setLight', -1, coords)
+    end)
+else
+    return
+end
